6208aafbc63db7d52d4fafa7cfe27b5ea0b0f1ba
Gabi Badescu <gabi_badescu99@yahoo.com>
2022-11-20 22:14:34 +0200

    project init

---
+++ FoodPlaner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodPlaner.Startup))]
+namespace FoodPlaner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
