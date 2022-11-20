23dab36219a0c27fa00e70fa848505fa1a1598a9
Gabi Badescu <gabi_badescu99@yahoo.com>
2022-11-20 22:59:15 +0200

    add module

---
+++ Project/FoodPlaner/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodPlaner.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int RecipeId { get; set; }
+        public int UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public string Ingredients { get; set; }
+        public int Time { get; set; }
+        public bool Intolerances { get; set; }
+        public string Cuisine { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ Project/FoodPlaner/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodPlaner.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public int ReceipeId { get; set; }
+        public int UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public float Score { get; set; }
+        [Required(ErrorMessage = "Continutul comentariului dumneavoastra este obligatoriu!")]
+        public string Text { get; set; }
+        public virtual Recipe Recipe { get; set; }
+    }
+}
