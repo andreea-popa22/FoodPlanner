76589f82d9d4860d062bdad3b35572c7d9932225
Gabi Badescu <gabi_badescu99@yahoo.com>
2022-12-04 23:59:14 +0200

    put user attributes into identity model

--- Project/FoodPlaner/Models/IdentityModels.cs
+++ Project/FoodPlaner/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,14 @@ namespace FoodPlaner.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public float BodyIndex { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+        public virtual ICollection<Recipe> Recipes { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
