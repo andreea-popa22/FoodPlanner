88311b05a5a77d9c2808b3292530fcc78dd9d332
alina0311 <61750022+alina0311@users.noreply.github.com>
2022-12-11 22:31:16 +0200

    reviewController implementation addendum

--- Project/FoodPlaner/Controllers/ReviewController.cs
+++ Project/FoodPlaner/Controllers/ReviewController.cs
@@ -37,19 +37,49 @@ namespace FoodPlaner.Controllers
         [HttpPost]
         public ActionResult New(Review review, int recipeId)
         {
-            return View();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    review.UserId = User.Identity.GetUserId();
+                    review.RecipeId = recipeId;
+                  
+                    review.Date = DateTime.Now;
+                    reviewRepository.InsertReview(review);
+                    reviewRepository.Save();
+                    return Redirect("/Recipe/Show/" + recipeId);
+                }
+                else
+                {
+                    return Redirect("/Recipe/Show/" + recipeId);
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Redirect("/Recipe/Show/" + recipeId);
+            }
         }
 
         // GET: Recipe
-        public ActionResult Show(int id)
-        {
-            return View();
+        public ActionResult Show(int reviewId)
+        { 
+            Review review = reviewRepository.GetReviewByID(reviewId);
+            ApplicationUser user = reviewRepository.GetUserByReviewID(review.UserId);
+
+            ViewBag.userName = user.Name + " " + user.Surname;
+            return View(review);
         }
 
         [HttpDelete]
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int reviewId)
         {
-            return View();
+            Review review = reviewRepository.GetReviewByID(reviewId);
+            Recipe recipe = reviewRepository.GetRecipeByReviewID(review.RecipeId);
+            reviewRepository.DeleteReview(reviewId);
+            reviewRepository.Save();
+
+            return Redirect("/Recipe/Show/" + recipe.RecipeId);
         }
         protected override void Dispose(bool disposing)
         {
